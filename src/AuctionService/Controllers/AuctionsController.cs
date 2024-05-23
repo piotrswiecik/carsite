bafd33f1ec878f769b6c0bedc70f783c602fce91
Piotr Święcik <piotr.swiecik@gmail.com>
2024-05-23 21:09:48 +0200

    basic controller endpoints

---
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -0,0 +1,42 @@
+using AuctionService.Data;
+using AuctionService.DTO;
+using AuctionService.Models;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService.Controllers;
+
+[ApiController]
+[Route("api/auctions")]
+public class AuctionsController(AuctionDbContext ctx, IMapper mapper) : ControllerBase
+{
+    private readonly AuctionDbContext _ctx = ctx;
+    private readonly IMapper _mapper = mapper;
+
+    [HttpGet]
+    public async Task<ActionResult<List<AuctionDto>>> GetAllAuctions()
+    {
+        var auctions = await _ctx.Auctions
+            .Include(x => x.Item)
+            .OrderBy(x => x.Item.Make)
+            .ToListAsync();
+        
+        return _mapper.Map<List<AuctionDto>>(auctions);
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<AuctionDto>> GetAuctionById(Guid id)
+    {
+        var auction = await _ctx.Auctions
+            .Include(x => x.Item)
+            .FirstOrDefaultAsync(x => x.Id == id);
+
+        if (auction == null)
+        {
+            return NotFound();
+        }
+
+        return _mapper.Map<AuctionDto>(auction);
+    }
+}
