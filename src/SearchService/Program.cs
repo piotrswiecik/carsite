ef50fa643307383a1392950ee3b7c7cdd8b76fb3
Piotr Święcik <piotr.swiecik@gmail.com>
2024-05-24 11:10:29 +0200

    mongodb config

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,3 +1,7 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -18,5 +22,14 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+await DB.InitAsync("SearchDb", MongoClientSettings.FromConnectionString(
+    builder.Configuration.GetConnectionString("MongoDB")));
+
+await DB.Index<Item>()
+    .Key(x => x.Make, KeyType.Text)
+    .Key(x => x.Model, KeyType.Text)
+    .Key(x => x.Color, KeyType.Text)
+    .CreateAsync();
 
 app.Run();
