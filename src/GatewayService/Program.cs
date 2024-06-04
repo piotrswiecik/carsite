cd951da4d624575984b3b4a84758d6f38d62110f
Piotr Święcik <piotr.swiecik@gmail.com>
2024-06-04 21:34:45 +0200

    yarp auth config

--- src/GatewayService/Program.cs
+++ src/GatewayService/Program.cs
@@ -1,10 +1,25 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+// gateway service is able to terminate jwt
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(opt =>
+    {
+        opt.Authority = builder.Configuration["IdentityService:Authority"];
+        opt.RequireHttpsMetadata = false;
+        opt.TokenValidationParameters.ValidateAudience = false;
+        opt.TokenValidationParameters.NameClaimType = "username";
+    });
+
 var app = builder.Build();
 
 app.MapReverseProxy();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.Run();
