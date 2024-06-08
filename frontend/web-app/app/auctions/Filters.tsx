e4ea6f3377015c622882065bf1143b14d54f8bff
Piotr Święcik <piotr.swiecik@gmail.com>
2024-06-08 20:17:47 +0200

    refactor listings to use zustand store

--- frontend/web-app/app/auctions/Filters.tsx
+++ frontend/web-app/app/auctions/Filters.tsx
@@ -1,13 +1,12 @@
 import {Button, ButtonGroup} from "flowbite-react";
+import {useParamsStore} from "@/hooks/useParamsStore";
 
 const pageSizeButtons = [4, 8, 12];
 
-type Props = {
-    pageSize: number;
-    setPageSize: (size: number) => void;
-}
-
-export default function Filters({ pageSize, setPageSize }: Props) {
+export default function Filters() {
+    const pageSize = useParamsStore(state => state.pageSize);
+    const setParams = useParamsStore(state => state.setParams);
+    
     return (
         <div className="flex justify-between items-center mb-4">
             <div>
@@ -16,7 +15,7 @@ export default function Filters({ pageSize, setPageSize }: Props) {
                     {pageSizeButtons.map((value, index) => (
                         <Button 
                             key={index}
-                            onClick={() => setPageSize(value)}
+                            onClick={() => setParams({pageSize: value})}
                             color={`${pageSize === value ? "red" : "gray"}`}  //flowbite syntax
                             className="focus:ring-0"
                         >{value}</Button>
--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -3,39 +3,57 @@
 import AuctionCard from "@/app/auctions/AuctionCard";
 import AppPagination from "@/app/components/AppPagination";
 import {useEffect, useState} from "react";
-import {Auction} from "@/types";
+import {Auction, PagedResult} from "@/types";
 import {getData} from "@/app/actions/auctionActions";
 import Filters from "@/app/auctions/Filters";
+import {useParamsStore} from "@/hooks/useParamsStore";
+import {shallow} from "zustand/shallow";
+import qs from "query-string";
 
 
 export default function Listings() {
-    const [auctions, setAuctions] = useState<Auction[]>([]);
-    const [pageCount, setPageCount] = useState(0);
-    const [pageNumber, setPageNumber] = useState(1);
-    const [pageSize, setPageSize] = useState(4);
+    const [data, setData] = useState<PagedResult<Auction>>();
+    
+    // TODO: update to non-deprecated method
+    // this hook is used to get the current state of the store
+    // but we ensure that only required properties are returned
+    const params = useParamsStore(state => ({
+        pageNumber: state.pageNumber,
+        pageSize: state.pageSize,
+        searchTerm: state.searchTerm
+    }), shallow);
+    
+    const setParams = useParamsStore(state => state.setParams);
+    
+    // this helper will convert params retrieved from the store to a query string used for backend call
+    const url = qs.stringifyUrl({url: "", query: params});
+    
+    // set new pageNumber in store
+    function setPageNumber(pageNumber: number) {
+        setParams({pageNumber});
+    }
 
     // run once on initial page load + on page number change
     useEffect(() => {
-        getData(pageNumber, pageSize).then(data => {
-            setAuctions(data.results);
-            setPageCount(data.pageCount);
+        getData(url).then(data => {
+            setData(data);
         });
-    }, [pageNumber, pageSize]);
+    }, [url]);
     
-    if (auctions.length === 0) {
+    if (!data) {
         return <h3>Loading...</h3>
     }
     
     return (
         <>
-            <Filters pageSize={pageSize} setPageSize={setPageSize} />
+            <Filters />
             <div className="grid grid-cols-4 gap-6">
-                {auctions.map((auction) => (
+                {data.results.map((auction) => (
                     <AuctionCard auction={auction} key={auction.id}/>
                 ))}
             </div>
             <div className="flex justify-center mt-4">
-                <AppPagination currentPage={1} pageCount={pageCount} pageChanged={setPageNumber}/>
+                <AppPagination currentPage={params.pageNumber} pageCount={data.pageCount} pageChanged={setPageNumber}/>
             </div>
         </>
 
