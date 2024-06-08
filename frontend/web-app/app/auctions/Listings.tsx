7a456b552684099d7bed6ab6388649fa54311cd5
Piotr Święcik <piotr.swiecik@gmail.com>
2024-06-08 21:07:37 +0200

    restructure auctions page

--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -46,22 +46,24 @@ export default function Listings() {
     if (!data) {
         return <h3>Loading...</h3>
     }
-    
-    if (data.totalCount === 0) {
-        return <EmptyFilter showReset />
-    }
-    
+
     return (
         <>
             <Filters />
-            <div className="grid grid-cols-4 gap-6">
-                {data.results.map((auction) => (
-                    <AuctionCard auction={auction} key={auction.id}/>
-                ))}
-            </div>
-            <div className="flex justify-center mt-4">
-                <AppPagination currentPage={params.pageNumber} pageCount={data.pageCount} pageChanged={setPageNumber}/>
-            </div>
+            {data.totalCount === 0 ? (
+                <EmptyFilter showReset />
+            ) : (
+                <>
+                    <div className="grid grid-cols-4 gap-6">
+                        {data.results.map((auction) => (
+                            <AuctionCard auction={auction} key={auction.id}/>
+                        ))}
+                    </div>
+                    <div className="flex justify-center mt-4">
+                        <AppPagination currentPage={params.pageNumber} pageCount={data.pageCount} pageChanged={setPageNumber}/>
+                    </div>
+                </>
+            )}
         </>
 
     );
