31cb2f53c656b1d116fd6ac04a21e3aac40cb8c4
Piotr Święcik <piotr.swiecik@gmail.com>
2024-06-08 20:06:07 +0200

    set up zustand store

---
+++ frontend/web-app/hooks/useParamsStore.ts
@@ -0,0 +1,50 @@
+import {create} from "zustand";
+
+type State = {
+    pageSize: number;
+    pageNumber: number;
+    pageCount: number;
+    searchTerm: string;
+};
+
+type Actions = {
+  setParams: (params: Partial<State>) => void;
+  reset: () => void;
+};
+
+const initialState: State = {
+  pageNumber: 1,
+  pageSize: 12,
+  pageCount: 1,
+  searchTerm: ""  
+};
+
+export const useParamsStore = create<State & Actions>()((set) => ({
+    ...initialState,
+    
+    // provides implementation for setParams in Actions
+    // takes a partial State param and uses "set()" function provided by zustand to update state
+    setParams: (newParams) => {
+        // set takes a partial of State & Actions (so all properties of State & Actions are available but optional)
+        // it's supposed to process any state changes in any way and then return the new state as State typed object
+        set((state) => {
+            // if pageNumber was changed, simply update it in state
+            if (newParams.pageNumber) {
+                return {
+                    ...state,
+                    pageNumber: newParams.pageNumber
+                }
+            } else {
+                // if something else was changed (pageSize, searchTerm), update it in state and reset pageNumber to 1
+                // so for example after changing pageSize, we want to start from the first page anew
+                return {
+                    ...state,
+                    ...newParams,
+                    pageNumber: 1
+                }
+            }
+        });
+    },
+    
+    reset: () => set(initialState)
+}));
