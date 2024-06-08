5e4500ca7ee8aaa5046397af9625e2e7b863c1cd
Piotr Święcik <piotr.swiecik@gmail.com>
2024-06-08 10:28:37 +0200

    install flowbite

--- frontend/web-app/tailwind.config.ts
+++ frontend/web-app/tailwind.config.ts
@@ -1,25 +1,29 @@
-import type { Config } from "tailwindcss";
+import type {Config} from "tailwindcss";
+import flowbite from "flowbite-react/tailwind";
 
+/** @type {import('tailwindcss').Config} */
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+        flowbite.content()
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+                "gradient-conic":
+                    "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+            },
+        },
     },
-  },
-  corePlugins: {
-    aspectRatio: false,
-  },
-  plugins: [
-      require("@tailwindcss/aspect-ratio")
-  ],
+    corePlugins: {
+        aspectRatio: false,
+    },
+    plugins: [
+        require("@tailwindcss/aspect-ratio"),
+        flowbite.plugin()
+    ],
 };
 export default config;
