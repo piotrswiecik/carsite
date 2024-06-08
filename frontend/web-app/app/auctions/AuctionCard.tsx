b699921fe30e43a620fdc2d6eef5ac4110103951
Piotr Święcik <piotr.swiecik@gmail.com>
2024-06-08 09:31:25 +0200

    add auction card

--- frontend/web-app/app/auctions/AuctionCard.tsx
+++ frontend/web-app/app/auctions/AuctionCard.tsx
@@ -8,12 +8,21 @@ export default function AuctionCard({ auction }: Props) {
     return (
         <a href="#">
             <div className="w-full bg-gray-200 aspect-w-16 aspect-h-10 rounded-lg overflow-hidden">
-                <Image
-                    src={auction.imageUrl}
-                    alt="image"
-                    fill
-                    className="object-cover"
-                />
+                <div>
+                    <Image
+                        src={auction.imageUrl}
+                        alt="image"
+                        fill
+                        priority // Load image as soon as possible - no lazy loading
+                        className="object-cover"
+                        sizes="(max-width:768px) 100vw, (max-width:1200px) 50vw, 25vw"  // Responsive image sizes for optimal performance
+                    /> 
+                </div>
+                
+            </div>
+            <div className="flex justify-between items-center mt-4">
+                <h3 className="text-gray-700">{auction.make} {auction.model}</h3>
+                <p className="font-semibold text-sm">{auction.year}</p>
             </div>
         </a>
     )
