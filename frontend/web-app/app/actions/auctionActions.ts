51b8cdd977248c11263c404426093310fe24d74d
Piotr Święcik <piotr.swiecik@gmail.com>
2024-06-09 16:21:25 +0200

    test authorization

--- frontend/web-app/app/actions/auctionActions.ts
+++ frontend/web-app/app/actions/auctionActions.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import {Auction, PagedResult} from "@/types";
+import {getTokenWorkaround} from "@/app/actions/authActions";
 
 export async function getData(query: string): Promise<PagedResult<Auction>> {
     const res = await fetch(`http://localhost:6001/search${query}`);
@@ -17,9 +18,14 @@ export async function UpdateAuctionTest() {
         mileage: Math.floor(Math.random() * 100000 + 1)
     };
     
+    const token = await getTokenWorkaround();
+    
     const res = await fetch("http://localhost:6001/auctions/afbee524-5972-4075-8800-7d1f9d7b0a0c", {
         method: "PUT",
-        headers: {},
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token?.access_token}`
+        },
         body: JSON.stringify(data)
     });
     
