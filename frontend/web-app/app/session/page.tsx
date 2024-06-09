e0578797bc57fabbced7ee33f000a0f0abf0a09e
Piotr Święcik <piotr.swiecik@gmail.com>
2024-06-09 14:52:16 +0200

    getsession and getuser actions, session diag page

---
+++ frontend/web-app/app/session/page.tsx
@@ -0,0 +1,18 @@
+// for diagnostic purposes
+
+import {getSession} from "@/app/actions/authActions";
+import Heading from "@/app/components/Heading";
+
+export default async function SessionPage() {
+    const session = await getSession();
+    
+    return (
+        <div>
+            <Heading title="Session dashboard" subtitle="Powered by NextAuth.js" />
+            <div className="bg-blue-200 border-2 border-blue-500">
+                <h3 className="text-lg">Session data</h3>
+                <pre>{JSON.stringify(session, null, 2)}</pre>
+            </div>
+        </div>
+    );
+}
