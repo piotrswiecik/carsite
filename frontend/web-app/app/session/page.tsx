28932b291795bc433f2aa86f03ff1f7b554450a6
Piotr Święcik <piotr.swiecik@gmail.com>
2024-06-09 16:09:27 +0200

    add auth tester page to frontend

--- frontend/web-app/app/session/page.tsx
+++ frontend/web-app/app/session/page.tsx
@@ -2,6 +2,7 @@
 
 import {getSession} from "@/app/actions/authActions";
 import Heading from "@/app/components/Heading";
+import AuthTest from "@/app/session/AuthTest";
 
 export default async function SessionPage() {
     const session = await getSession();
@@ -12,6 +13,9 @@ export default async function SessionPage() {
             <div className="bg-blue-200 border-2 border-blue-500">
                 <h3 className="text-lg">Session data</h3>
                 <pre>{JSON.stringify(session, null, 2)}</pre>
+            </div>
+            <div className="mt-4">
+                <AuthTest />
             </div>
         </div>
     );
