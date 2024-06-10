7286fe114a101a4c6879b6ed57daa5a8a12e2e83
Piotr Święcik <piotr.swiecik@gmail.com>
2024-06-10 13:47:44 +0200

    expand auction create form

--- frontend/web-app/app/auctions/AuctionForm.tsx
+++ frontend/web-app/app/auctions/AuctionForm.tsx
@@ -11,16 +11,29 @@ export default function AuctionForm() {
         handleSubmit,
         setFocus,
         formState: {isSubmitting, isValid, isDirty, errors}
-    } = useForm();
-    
+    } = useForm({mode: "onTouched"});
+
     function onSubmit(data: FieldValues) {
         console.log(data);
     }
 
     return (
         <form className="flex flex-col mt-3" onSubmit={handleSubmit(onSubmit)}>
-            <Input label="Make" name="make" control={control} rules={{required: "Make is required"}} />
-            <Input label="Model" name="model" control={control} rules={{required: "Model is required"}} />
+            <Input label="Make" name="make" control={control} rules={{required: "Make is required"}}/>
+            <Input label="Model" name="model" control={control} rules={{required: "Model is required"}}/>
+            <Input label="Color" name="color" control={control} rules={{required: "Color is required"}}/>
+            <div className="grid grid-cols-2 gap-3">
+                <Input label="Year" name="year" type="number" control={control} rules={{required: "Year is required"}}/>
+                <Input label="Mileage" name="mileage" type="number" control={control}
+                       rules={{required: "Mileage is required"}}/>
+            </div>
+            <Input label="Image URL" name="imageUrl" control={control}
+                   rules={{required: "Image URL is required"}}/>
+            <div className="grid grid-cols-2 gap-3">
+                <Input label="Reserve price (enter 0 if none)" name="reservePrice" type="number" control={control} rules={{required: "Reserve price is required"}}/>
+                <Input label="Auction end date/time" name="auctionEnd" type="date" control={control}
+                       rules={{required: "Date is required"}}/>
+            </div>
             <div className="flex justify-between">
                 <Button outline color="gray">Cancel</Button>
                 <Button
