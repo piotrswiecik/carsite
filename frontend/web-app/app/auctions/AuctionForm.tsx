72c42f8a7824727f0f81ca5ba664fe54bde75961
Piotr Święcik <piotr.swiecik@gmail.com>
2024-06-10 13:43:16 +0200

    refactor for to use react form controller

--- frontend/web-app/app/auctions/AuctionForm.tsx
+++ frontend/web-app/app/auctions/AuctionForm.tsx
@@ -2,9 +2,11 @@
 
 import {FieldValues, useForm} from "react-hook-form";
 import {Button, TextInput} from "flowbite-react";
+import Input from "@/app/components/Input";
 
 export default function AuctionForm() {
     const {
+        control,
         register,
         handleSubmit,
         setFocus,
@@ -17,22 +19,8 @@ export default function AuctionForm() {
 
     return (
         <form className="flex flex-col mt-3" onSubmit={handleSubmit(onSubmit)}>
-            <div className="mb-3 block">
-                <TextInput
-                    {...register("make", {required: "Make is required"})}
-                    placeholder="Make"
-                    color={errors?.make && "failure"}
-                    helperText={errors?.make?.message as string}
-                />
-            </div>
-            <div className="mb-3 block">
-                <TextInput
-                    {...register("model", {required: "Model is required"})}
-                    placeholder="Model"
-                    color={errors?.model && "failure"}
-                    helperText={errors?.model?.message as string}
-                />
-            </div>
+            <Input label="Make" name="make" control={control} rules={{required: "Make is required"}} />
+            <Input label="Model" name="model" control={control} rules={{required: "Model is required"}} />
             <div className="flex justify-between">
                 <Button outline color="gray">Cancel</Button>
                 <Button
