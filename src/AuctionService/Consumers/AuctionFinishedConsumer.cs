dbf63a5b8801205fb17fa5982139ec92f65cebc9
Piotr Święcik <piotr.swiecik@gmail.com>
2024-06-10 19:57:41 +0200

    fix guid in consumer

--- src/AuctionService/Consumers/AuctionFinishedConsumer.cs
+++ src/AuctionService/Consumers/AuctionFinishedConsumer.cs
@@ -10,7 +10,8 @@ public class AuctionFinishedConsumer(AuctionDbContext dbContext) : IConsumer<Auc
     public async Task Consume(ConsumeContext<AuctionFinished> context)
     {
         Console.WriteLine("Consuming auction finished");
-        var auction = await dbContext.Auctions.FindAsync(context.Message.AuctionId);
+        var guid = Guid.Parse(context.Message.AuctionId);
+        var auction = await dbContext.Auctions.FindAsync(guid);
 
         if (auction == null)
         {
