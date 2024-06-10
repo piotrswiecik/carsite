a4e1571d3d7159ede1a59553642b3e0cb1e0be99
Piotr Święcik <piotr.swiecik@gmail.com>
2024-06-10 17:33:35 +0200

    add auction update link

--- frontend/web-app/app/auctions/details/[id]/page.tsx
+++ frontend/web-app/app/auctions/details/[id]/page.tsx
@@ -3,14 +3,23 @@ import Heading from "@/app/components/Heading";
 import CountdownTimer from "@/app/auctions/CountdownTimer";
 import CardImage from "@/app/auctions/CardImage";
 import DetailedSpecs from "@/app/auctions/details/[id]/DetailedSpecs";
+import {getCurrentUser} from "@/app/actions/authActions";
+import EditButton from "@/app/auctions/details/[id]/EditButton";
 
 export default async function Details({params}: {params: {id: string}}) {
     const data = await getDetailedViewData(params.id);
+    const user = await getCurrentUser();
     
     return (
         <div>
             <div className="flex justify-between">
-                <Heading title={`${data.make} ${data.model}`} subtitle="" />
+                <div className="flex items-center gap-3">
+                    <Heading title={`${data.make} ${data.model}`} subtitle="" />
+                    {user?.username === data.seller && (
+                        <EditButton id={data.id} />
+                    )}
+                </div>
+                
                 <div className="flex gap-3 items-center">
                     <h3 className="text-2xl font-semibold">Time remaining: </h3>
                     <CountdownTimer auctionEnd={data.auctionEnd} />
