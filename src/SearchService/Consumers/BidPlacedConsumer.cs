219e0c67d41e548990f67bb5ccc62d63229057f9
Piotr Święcik <piotr.swiecik@gmail.com>
2024-06-10 19:59:32 +0200

    fix bid consumer in search svc

--- src/SearchService/Consumers/BidPlacedConsumer.cs
+++ src/SearchService/Consumers/BidPlacedConsumer.cs
@@ -23,6 +23,7 @@ public class BidPlacedConsumer : IConsumer<BidPlaced>
             && context.Message.Amount > auction.CurrentHighBid)
         {
             auction.CurrentHighBid = context.Message.Amount;
+            await auction.SaveAsync();
         }
     }
 }
