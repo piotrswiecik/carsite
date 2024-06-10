0f931e469d811855626649edb3e79e2ee57a2235
Piotr Święcik <piotr.swiecik@gmail.com>
2024-06-10 14:25:46 +0200

    test fetch wrapper for put

--- frontend/web-app/app/actions/auctionActions.ts
+++ frontend/web-app/app/actions/auctionActions.ts
@@ -8,23 +8,29 @@ export async function getData(query: string): Promise<PagedResult<Auction>> {
     return await fetchWrapper.get(`/search${query}`);
 }
 
-export async function UpdateAuctionTest() {
+export async function updateAuctionTest() {
+    // const data = {
+    //     mileage: Math.floor(Math.random() * 100000 + 1)
+    // };
+    //
+    // const token = await getTokenWorkaround();
+    //
+    // const res = await fetch("http://localhost:6001/auctions/afbee524-5972-4075-8800-7d1f9d7b0a0c", {
+    //     method: "PUT",
+    //     headers: {
+    //         "Content-Type": "application/json",
+    //         "Authorization": `Bearer ${token?.access_token}`
+    //     },
+    //     body: JSON.stringify(data)
+    // });
+    //
+    // if (!res.ok) return {status: res.status, message: res.statusText};
+    //
+    // return res.statusText;
+
     const data = {
         mileage: Math.floor(Math.random() * 100000 + 1)
     };
-    
-    const token = await getTokenWorkaround();
-    
-    const res = await fetch("http://localhost:6001/auctions/afbee524-5972-4075-8800-7d1f9d7b0a0c", {
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${token?.access_token}`
-        },
-        body: JSON.stringify(data)
-    });
-    
-    if (!res.ok) return {status: res.status, message: res.statusText};
-    
-    return res.statusText;
+    return await fetchWrapper.put("/auctions/afbee524-5972-4075-8800-7d1f9d7b0a0c", data);
 }
+
