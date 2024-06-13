583b7cfb5a238c1524ceb063bae163ec8294ba0b
Piotr Święcik <piotr.swiecik@gmail.com>
2024-06-13 18:20:40 +0200

    fix: reject low bids

--- frontend/web-app/app/auctions/details/[id]/BidForm.tsx
+++ frontend/web-app/app/auctions/details/[id]/BidForm.tsx
@@ -16,6 +16,9 @@ export default function BidForm({auctionId, highBid}: Props) {
     const addBid = useBidStore(state => state.addBid);
     
     function onSubmit(data: FieldValues) {
+        if (data.amount < highBid) {
+            return toast.error("Bid must be at least " + numberWithCommas(highBid + 1));
+        }
         placeBidForAuction(auctionId, +data.amount)
             .then(bid => {
                 if (bid.error) {
